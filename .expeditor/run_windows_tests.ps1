4274c8f317f58eba536a11a244c7d3f435fe941e
nitin sanghi <nsanghi@progress.com>
2025-04-11 18:08:38 +0530

    using ruby dev kit for chef  install
    Signed-off-by: nitin sanghi <nsanghi@progress.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -1,12 +1,20 @@
 $ErrorActionPreference="stop"
 Add-WindowsCapability -Online -Name OpenSSH.Client
 Write-Host "--- bundle install"
+ridk install 3
+gem install git
+
+
+
 bundle config build.chef --without-win32-event-log
 bundle config --local path vendor/bundle
+Write-Host "--- cloning chef for  install"
+
 git clone https://github.com/chef/chef.git
 cd chef ; bundle install; rake install ; cd ..
 bundle config set --local without docs development profile
 bundle install --jobs=7 --retry=3
+Write-Host "--- bundle  install done"
 
 
 Write-Host "+++ bundle exec task"
