f60fe29b5cfd52968ecbc08558d6e76e1f6a3671
nitin sanghi <nsanghi@progress.com>
2025-04-11 17:38:18 +0530

    Trying openssl in windows pipeline
    Signed-off-by: nitin sanghi <nsanghi@progress.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference="stop"
-
+Add-WindowsCapability -Online -Name OpenSSH.Client
 Write-Host "--- bundle install"
 bundle config build.chef --without-win32-event-log
 bundle config --local path vendor/bundle
@@ -7,9 +7,6 @@ git clone https://github.com/chef/chef.git
 cd chef ; bundle install; rake install ; cd ..
 bundle config set --local without docs development profile
 bundle install --jobs=7 --retry=3
-
-Write-Host "--- bundle install again"
-bundle install
 
 
 Write-Host "+++ bundle exec task"
