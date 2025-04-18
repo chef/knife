7e0460f33dda6cdc414e3051af7641ab2d655f05
nitin sanghi <nsanghi@progress.com>
2025-04-18 17:49:27 +0530

    removed Gemfile.lock and trying to install chef
    Signed-off-by: nitin sanghi <nsanghi@progress.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -1,18 +1,12 @@
 $ErrorActionPreference="stop"
-Add-WindowsCapability -Online -Name OpenSSH.Client
 Write-Host "--- bundle install"
 ridk install 3
-gem install git
-
-
-
 bundle config --local path vendor/bundle
 Write-Host "--- cloning chef for  install"
-
 git clone https://github.com/chef/chef.git
 cd chef ; bundle install; cd chef-utils; gem build chef-utils.gemspec; gem install chef-utils-*.gem ; cd .. ;
 cd chef-config; gem build chef-config.gemspec; gem install chef-config-*.gem ; cd ..;
- gem build chef-universal-mingw-ucrt.gemspec; gem install chef-*.gem ; cd ..;
+gem build chef-universal-mingw-ucrt.gemspec; gem install chef-*.gem ; cd ..;
 
 bundle config set --local without docs development profile
 bundle install --jobs=7 --retry=3
