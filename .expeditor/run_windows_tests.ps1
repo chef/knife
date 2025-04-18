261800db6b9199cd288cbca20e6fa425fef078d0
nitin sanghi <nsanghi@progress.com>
2025-04-18 12:21:16 +0530

    Trying to build and install chef gem in windows
    Signed-off-by: nitin sanghi <nsanghi@progress.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -10,7 +10,10 @@ bundle config --local path vendor/bundle
 Write-Host "--- cloning chef for  install"
 
 git clone https://github.com/chef/chef.git
-cd chef ; bundle install; rake install ; cd ..
+cd chef ; bundle install; cd chef-config; gem build chef-config.gemspec; gem install chef-config-*.gem ; cd ..
+cd chef ; cd chef-utils; gem build chef-utils.gemspec; gem install chef-utils-*.gem ; cd ..
+cd chef ; gem build chef-universal-mingw-ucrt.gemspec; gem install chef-*.gem ; cd ..
+
 bundle config set --local without docs development profile
 bundle install --jobs=7 --retry=3
 Write-Host "--- bundle  install done"
