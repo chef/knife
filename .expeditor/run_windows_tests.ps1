918a37d5ca7d9c05e5a806d3b6ffb6d5509d0ff5
nitin sanghi <nsanghi@progress.com>
2025-04-21 11:31:39 +0530

    removed extra flag in bundle install
    Signed-off-by: nitin sanghi <nsanghi@progress.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -4,17 +4,16 @@ ridk install 1 2 3
 ridk enable
 bundle config --local path vendor/bundle
 Write-Host "--- cloning chef for  install"
-git clone https://github.com/chef/chef.git
-cd chef ; bundle install; cd chef-utils; gem build chef-utils.gemspec; gem install chef-utils-*.gem ; cd .. ;
-cd chef-config; gem build chef-config.gemspec; gem install chef-config-*.gem ; cd ..;
-gem build chef-universal-mingw-ucrt.gemspec; gem install chef-*.gem ; cd ..;
+#git clone https://github.com/chef/chef.git
+#cd chef ; bundle install; cd chef-utils; gem build chef-utils.gemspec; gem install chef-utils-*.gem ; cd .. ;
+#cd chef-config; gem build chef-config.gemspec; gem install chef-config-*.gem ; cd ..;
+#gem build chef-universal-mingw-ucrt.gemspec; gem install chef-*.gem ; cd ..;
 gem install chef-utils
 gem install win32ole
 gem install ffi-libarchive
 Write-Host "--- checking current Dir"
 
 Write-Host "--- checking current Dir"
-bundle config set --local without docs development profile
 bundle install --jobs=7 --retry=3
 Write-Host "--- bundle  install done"
 
