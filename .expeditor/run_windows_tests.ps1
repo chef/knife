0ff5a82729f2b2a1f4091d79054102432dbf13c5
nitin sanghi <nsanghi@progress.com>
2025-04-21 10:38:32 +0530

    Change gemfile lock working on windows
    Signed-off-by: nitin sanghi <nsanghi@progress.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -3,14 +3,13 @@ Write-Host "--- bundle install"
 ridk install 3
 bundle config --local path vendor/bundle
 Write-Host "--- cloning chef for  install"
-git clone https://github.com/chef/chef.git
-cd chef ; bundle install; cd chef-utils; gem build chef-utils.gemspec; gem install chef-utils-*.gem ; cd .. ;
-cd chef-config; gem build chef-config.gemspec; gem install chef-config-*.gem ; cd ..;
-gem build chef-universal-mingw-ucrt.gemspec; gem install chef-*.gem ; cd ..;
+#git clone https://github.com/chef/chef.git
+#cd chef ; bundle install; cd chef-utils; gem build chef-utils.gemspec; gem install chef-utils-*.gem ; cd .. ;
+#cd chef-config; gem build chef-config.gemspec; gem install chef-config-*.gem ; cd ..;
+#gem build chef-universal-mingw-ucrt.gemspec; gem install chef-*.gem ; cd ..;
 
 Write-Host "--- checking current Dir"
-dir
-pwd
+
 Write-Host "--- checking current Dir"
 bundle config set --local without docs development profile
 bundle install --jobs=7 --retry=3
--- Gemfile
+++ Gemfile
@@ -33,3 +33,7 @@ gem "chef-utils", git: "https://github.com/chef/chef", branch: "main", glob: "ch
 # gem "chef", path: "../chef"
 # gem "chef-utils",
 # gem "chef-config", path: File.expand_path("../chef/chef-config", __dir__) if File.exist?(File.expand_path("../chef/chef-config", __dir__))
+
+platforms :mswin, :mingw, :x64_mingw do
+  gem "fiddle"
+end
