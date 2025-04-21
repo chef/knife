5aca7b55ebb167da541be655aafffb986079365a
nitin sanghi <nsanghi@progress.com>
2025-04-21 11:08:55 +0530

    installing chef manually
    Signed-off-by: nitin sanghi <nsanghi@progress.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -1,12 +1,13 @@
 $ErrorActionPreference="stop"
 Write-Host "--- bundle install"
-ridk install 1 2 3 --silent
+ridk install 1 2 3
+ridk enable
 bundle config --local path vendor/bundle
 Write-Host "--- cloning chef for  install"
-#git clone https://github.com/chef/chef.git
-#cd chef ; bundle install; cd chef-utils; gem build chef-utils.gemspec; gem install chef-utils-*.gem ; cd .. ;
-#cd chef-config; gem build chef-config.gemspec; gem install chef-config-*.gem ; cd ..;
-#gem build chef-universal-mingw-ucrt.gemspec; gem install chef-*.gem ; cd ..;
+git clone https://github.com/chef/chef.git
+cd chef ; bundle install; cd chef-utils; gem build chef-utils.gemspec; gem install chef-utils-*.gem ; cd .. ;
+cd chef-config; gem build chef-config.gemspec; gem install chef-config-*.gem ; cd ..;
+gem build chef-universal-mingw-ucrt.gemspec; gem install chef-*.gem ; cd ..;
 
 Write-Host "--- checking current Dir"
 
