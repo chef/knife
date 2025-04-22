9dd34fc3eadde4dc4229fefb4ae19f304b31f626
nikhil2611 <nikhilgupta2102@gmail.com>
2025-04-22 15:35:57 +0530

    updated the windows test file
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -1,21 +1,11 @@
 $ErrorActionPreference="stop"
+
 Write-Host "--- bundle install"
-#ridk install 1 2 3
-#ridk enable
-Write-Host "--- cloning chef for  install"
-#git clone https://github.com/chef/chef.git
-#cd chef ; bundle install; cd chef-utils; gem build chef-utils.gemspec; gem install chef-utils-*.gem ; cd .. ;
-#cd chef-config; gem build chef-config.gemspec; gem install chef-config-*.gem ; cd ..;
-#gem build chef-universal-mingw-ucrt.gemspec; gem install chef-*.gem ; cd ..;
+
 bundle config --local path vendor/bundle
-gem install win32ole
-# gem install ffi-libarchive
+bundle config set --local without docs development profile
 bundle install --jobs=7 --retry=3
-Write-Host "--- bundle  install done"
 
 Write-Host "+++ bundle exec task"
-
-
-
 bundle exec $args
 if ($LASTEXITCODE -ne 0) { throw "$args failed" }
