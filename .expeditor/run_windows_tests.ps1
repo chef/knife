b209dc9cd264c999956d0a454529c3598ee70494
nikhil2611 <nikhilgupta2102@gmail.com>
2025-05-06 18:39:57 +0530

    using fiddle <= 1.1.6
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -2,14 +2,14 @@ $ErrorActionPreference="stop"
 Write-Host "--- bundle install"
 #ridk install 1 2 3
 #ridk enable
-Write-Host "--- cloning chef for  install"
+# Write-Host "--- cloning chef for  install"
 #git clone https://github.com/chef/chef.git
 #cd chef ; bundle install; cd chef-utils; gem build chef-utils.gemspec; gem install chef-utils-*.gem ; cd .. ;
 #cd chef-config; gem build chef-config.gemspec; gem install chef-config-*.gem ; cd ..;
 #gem build chef-universal-mingw-ucrt.gemspec; gem install chef-*.gem ; cd ..;
 bundle config --local path vendor/bundle
 gem install win32ole
-# gem install ffi-libarchive
+gem install ffi-libarchive
 bundle install --jobs=7 --retry=3
 Write-Host "--- bundle  install done"
 
