cfbd709a3aa85828c0d735791fddfb1ec30c06e3
nikhil2611 <nikhilgupta2102@gmail.com>
2025-05-06 20:48:40 +0530

    added chef-powershell gm
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -10,6 +10,7 @@ Write-Host "--- bundle install"
 bundle config --local path vendor/bundle
 gem install win32ole
 gem install ffi-libarchive
+gem install chef-powershell
 bundle install --jobs=7 --retry=3
 Write-Host "--- bundle  install done"
 
