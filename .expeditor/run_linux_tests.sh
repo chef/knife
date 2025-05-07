ea920eb9b1876fcdde749cedb019c590c50e3925
Sean Simmons <sean.simmons@progress.com>
2025-05-07 16:06:02 -0400

    testing secret value
    Signed-off-by: Sean Simmons <sean.simmons@progress.com>

--- .expeditor/run_linux_tests.sh
+++ .expeditor/run_linux_tests.sh
@@ -4,10 +4,6 @@
 
 set -ue
 
-echo "---- printing aws version"
-aws --version
-echo "---- printing aws version"
-
 export USER="root"
 export LANG=C.UTF-8 LANGUAGE=C.UTF-8
 echo "---- getting chef gem"
