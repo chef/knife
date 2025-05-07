71c48c72bf81db9e29b5cd131522ab942dca4a73
Sean Simmons <sean.simmons@progress.com>
2025-05-07 16:17:16 -0400

    testing secret value
    Signed-off-by: Sean Simmons <sean.simmons@progress.com>

--- .expeditor/run_linux_tests.sh
+++ .expeditor/run_linux_tests.sh
@@ -3,17 +3,18 @@
 # This script runs a passed in command, but first setups up the bundler caching on the repo
 
 set -ue
-printenv
+
 export USER="root"
 export LANG=C.UTF-8 LANGUAGE=C.UTF-8
 echo "---- getting chef gem"
 export ARTIFACTORY_BUILDKITE_TOKEN_PIPELINE="${ARTIFACTORY_BUILDKITE_TOKEN}"
 export ARTIFACTORY_ENDPOINT="https://artifactory-internal.ps.chef.co/artifactory"
 export ARTIFACTORY_USERNAME="buildkite"
+ARTIFACTORY_TOKEN=$(vault kv get -field token account/static/artifactory/buildkite)
 echo "--- gem source before add"
 gem source
 echo "--- gem source after add"
-gem source -a https://buildkite:$ARTIFACTORY_BUILDKITE_TOKEN_PIPELINE@artifactory-internal.ps.chef.co/artifactory/api/gems/omnibus-gems-local/
+gem source -a https://buildkite:$ARTIFACTORY_TOKEN@artifactory-internal.ps.chef.co/artifactory/api/gems/omnibus-gems-local/
 gem source
 echo  "---- getting chef gem done"
 echo "--- bundle install"
