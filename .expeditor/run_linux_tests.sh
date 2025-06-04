7f951550817ba656977fb6c5a49e248785d6e6d2
muthuja <muthujabavaji.vempalli@progress.com>
2025-06-04 12:59:43 +0530

    add few bundle commands
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- .expeditor/run_linux_tests.sh
+++ .expeditor/run_linux_tests.sh
@@ -34,7 +34,6 @@ echo "--- gem source before add"
 gem source
 echo "--- gem source after add"
 gem source -a https://artifactory-internal.ps.chef.co/artifactory/omnibus-gems-local/gems
-gem source
 echo  "---- getting chef gem done"
 echo "--- bundle install"
 bundle config --local set --local deployment 'true'
