b2d65739d1015594c426b3ba9fed1fe9e80b2fd7
muthuja <muthujabavaji.vempalli@progress.com>
2025-06-05 15:51:17 +0530

    test with no artifactory password for win
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -7,17 +7,17 @@ Write-Host "---- getting chef gem"
 # Ensure unzip (Expand-Archive) and Vault are available
 
 # Install Vault if not present
-if (-not (Get-Command vault -ErrorAction SilentlyContinue)) {
-    Write-Host "--- installing vault"
-    $vaultVersion = "1.8.2"
-    $vaultZip = "vault_${vaultVersion}_windows_amd64.zip"
-    $vaultUrl = "https://releases.hashicorp.com/vault/$vaultVersion/$vaultZip"
-    $vaultTmp = "$env:TEMP\$vaultZip"
-    Invoke-WebRequest -Uri $vaultUrl -OutFile $vaultTmp
-    Expand-Archive -Path $vaultTmp -DestinationPath $env:TEMP -Force
-    Move-Item -Path "$env:TEMP\vault.exe" -Destination "C:\Windows\System32\vault.exe" -Force
-    Remove-Item $vaultTmp
-}
+# if (-not (Get-Command vault -ErrorAction SilentlyContinue)) {
+#     Write-Host "--- installing vault"
+#     $vaultVersion = "1.8.2"
+#     $vaultZip = "vault_${vaultVersion}_windows_amd64.zip"
+#     $vaultUrl = "https://releases.hashicorp.com/vault/$vaultVersion/$vaultZip"
+#     $vaultTmp = "$env:TEMP\$vaultZip"
+#     Invoke-WebRequest -Uri $vaultUrl -OutFile $vaultTmp
+#     Expand-Archive -Path $vaultTmp -DestinationPath $env:TEMP -Force
+#     Move-Item -Path "$env:TEMP\vault.exe" -Destination "C:\Windows\System32\vault.exe" -Force
+#     Remove-Item $vaultTmp
+# }
 
 # Expand-Archive is built-in on modern PowerShell/Windows, so unzip is not needed separately.
 # If you need to unzip other files, use Expand-Archive:
@@ -26,7 +26,7 @@ if (-not (Get-Command vault -ErrorAction SilentlyContinue)) {
 $env:ARTIFACTORY_ENDPOINT="artifactory-internal.ps.chef.co/artifactory"
 $env:ARTIFACTORY_USERNAME="REDACTED@chef.io"
 #$lita_password=aws ssm get-parameter --name "artifactory-lita-password" --with-decryption --query Parameter.Value --output text --region us-west-2
-$env:ARTIFACTORY_PASSWORD="$(vault read -field password account/static/artifactory/buildkite)"
+#$env:ARTIFACTORY_PASSWORD="$(vault read -field password account/static/artifactory/buildkite)"
 Write-Host "---- getting chef gem done"
 # git clone https://github.com/chef/chef.git
 # cd chef ; bundle install; cd chef-utils; gem build chef-utils.gemspec; gem install chef-utils-*.gem ; cd .. ;
