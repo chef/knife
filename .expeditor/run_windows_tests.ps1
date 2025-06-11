283666f0861905999d3cc8111302ba56cc5cdf85
muthuja <muthujabavaji.vempalli@progress.com>
2025-06-11 11:57:20 +0530

    change the windows execution script
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference="stop"
-Write-Host "---- getting chef gem"
+
 
 $env:ARTIFACTORY_ENDPOINT="artifactory-internal.ps.chef.co/artifactory"
 $env:ARTIFACTORY_USERNAME="REDACTED@chef.io"
@@ -9,17 +9,12 @@ $env:ARTIFACTORY_USERNAME="REDACTED@chef.io"
 # cd chef ; bundle install; cd chef-utils; gem build chef-utils.gemspec; gem install chef-utils-*.gem ; cd .. ;
 # cd chef-config; gem build chef-config.gemspec; gem install chef-config-*.gem ; cd ..;
 # gem build chef-universal-mingw-ucrt.gemspec; gem install chef-*.gem ; cd ..;
+Write-Host "--- bundle install"
+
 bundle config --local path vendor/bundle
-gem install win32ole
-gem install ffi-libarchive
+bundle config set --local without docs development profile
 bundle install --jobs=7 --retry=3
-bundle install --deployment
-bundle config unset deployment
-bundle --full-index
-Write-Host "--- bundle  install done"
-Write-Host "---- getting chef gem done"
 
 Write-Host "+++ bundle exec task"
-
-$env:RUBYOPT="-W0"; bundle exec $args
+bundle exec $args
 if ($LASTEXITCODE -ne 0) { throw "$args failed" }
