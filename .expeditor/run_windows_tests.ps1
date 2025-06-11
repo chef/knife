babc51a7b6abadc5ceaea89145b9cf0e89f8085a
muthuja <muthujabavaji.vempalli@progress.com>
2025-06-11 13:42:28 +0530

    change the execution script
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -14,6 +14,7 @@ Write-Host "--- bundle install"
 # Set bundler config
 bundle config --local path vendor/bundle
 bundle config set --local without 'docs development profile'
+bundle config set --local disable_checksum_validation true
 
 # Install gems
 bundle install --jobs=7 --retry=3 --verbose
