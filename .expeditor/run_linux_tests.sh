5f62c6cf80c6dc673a9c2b98df3d2342853dfa42
nikhil2611 <nikhilgupta2102@gmail.com>
2025-07-21 20:46:16 +0530

    trying to fix ruby 3.1 pipeline linux
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- .expeditor/run_linux_tests.sh
+++ .expeditor/run_linux_tests.sh
@@ -13,6 +13,18 @@ export LANG=C.UTF-8 LANGUAGE=C.UTF-8
 export ARTIFACTORY_ENDPOINT="https://artifactory-internal.ps.chef.co/artifactory"
 export ARTIFACTORY_USERNAME="REDACTED@chef.io"
 
+ruby_version=$(ruby -e 'puts RUBY_VERSION')
+echo "--- Detected Ruby version: $ruby_version"
+
+# Fix bundler double load on Ruby 3.1 by installing compatible RubyGems + Bundler
+if [[ "$ruby_version" == 3.1* ]]; then
+  echo "--- Installing RubyGems 3.6.9 for Ruby 3.1"
+  gem install rubygems-update -v 3.6.9
+  update_rubygems
+  echo "--- Installing Bundler 2.6.9 for Ruby 3.1"
+  gem install bundler -v 2.6.9
+fi
+
 echo "--- Adding Artifactory gem source"
 gem source -a "${ARTIFACTORY_ENDPOINT}/api/gems/omnibus-gems-local" || true
 
