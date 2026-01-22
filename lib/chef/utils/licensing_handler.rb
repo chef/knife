bf74afec009f74f871bb4e9d722f68d1b7f9d389
nikhil2611 <ngupta@progress.com>
2026-01-22 20:28:32 +0530

    updated call to fetch_and_persist to fix bootstrap issue
    Signed-off-by: nikhil2611 <ngupta@progress.com>

--- lib/chef/utils/licensing_handler.rb
+++ lib/chef/utils/licensing_handler.rb
@@ -4,8 +4,8 @@ class Chef
   class Utils
     class LicensingHandler
       OMNITRUCK_URLS = {
-        "free" => "https://omnitruck.chef.io",
-        "trial" => "https://omnitruck.chef.io",
+        "free"       => "https://omnitruck.chef.io",
+        "trial"      => "https://omnitruck.chef.io",
         "commercial" => "https://chefdownload-commercial.chef.io",
       }.freeze
 
@@ -33,7 +33,7 @@ class Chef
       class << self
         def validate!
           license_keys = begin
-                           ChefLicensing::LicenseKeyFetcher.fetch
+                           ChefLicensing.fetch_and_persist
                          # If the env is airgapped or the local licensing service is unreachable,
                          # the licensing gem will raise ChefLicensing::RestfulClientConnectionError.
                          # In such cases, we are assuming the license is not available.
