8697b44451b0ac1db9368c9b79ab2efccb8ed2e4
nitin sanghi <nsanghi@progress.com>
2026-05-25 15:20:17 +0530

    Removed extra gem group getting installed during hab build
    Signed-off-by: nitin sanghi <nsanghi@progress.com>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -64,7 +64,7 @@ do_build() {
   build_line "Building the Knife gem from the gemspec with GEM_HOME=$GEM_HOME and GEM_PATH=$GEM_PATH"
   pushd "$HAB_CACHE_SRC_PATH/$pkg_dirname"
     bundle config set --local path "$GEM_HOME"
-    bundle config --local without integration deploy maintenance
+    bundle config --local without integration deploy maintenance development omnibus_package test
     bundle config --local jobs 4
     bundle config --local retry 5
     bundle config --local silence_root_warning 1
