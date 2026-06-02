bfd2b68a04760f21db15815472172cd5663fb330
nitin sanghi <nsanghi@progress.com>
2026-06-02 17:34:46 +0530

    Fix missing closing brace in Install-Habitat function
    Signed-off-by: nitin sanghi <nsanghi@progress.com>

--- .expeditor/buildkite/artifact.habitat.test.ps1
+++ .expeditor/buildkite/artifact.habitat.test.ps1
@@ -29,6 +29,8 @@ function Stop-HabProcess {
 function Install-Habitat {
   Write-Host "Downloading and installing Habitat..."
   Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.ps1'))
+}
+
 try {
   hab --version
 }
